c461f29bf26ac02e750c5c1b66061b0e8bb7f555
Rabia Terzi <rabiaterzi98@gmail.com>
2020-05-10 14:04:36 +0300

    ana sayfa

---
+++ DRxamarin/DRxamarin/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DRxamarin
+{
+	// Learn more about making custom code visible in the Xamarin.Forms previewer
+	// by visiting https://aka.ms/xamarinforms-previewer
+	[DesignTimeVisible(false)]
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+		//https://cdn0.iconfinder.com/data/icons/slim-square-icons-basics/100/basics-12-128.png
+		async void uyegirisi(object o,EventArgs args)
+		{
+			await Navigation.PushModalAsync(new uyegirisi());
+		}
+	}
+}
---
+++ DRxamarin/DRxamarin/uyegirisi.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DRxamarin
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class uyegirisi : ContentPage
+	{
+		public uyegirisi()
+		{
+			InitializeComponent();
+		}
+	}
+}
